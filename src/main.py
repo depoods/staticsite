45e199a14b323e16d242c3e2f16fcc82f5e44faa
depoods <daniel.hamler@gmail.com>
2024-04-28 20:36:31 +0200

    minor testing

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 from textnode import TextNode
 import os
+import shutil
 
 def main():
-    
+    file_copy("/home/depood/workspace/github.com/depoods/staticsite/static/index.css", "/home/depood/workspace/github.com/depoods/staticsite/public/index.css")
+    file_delete("/home/depood/workspace/github.com/depoods/staticsite/public/delete_me")
+
+def file_copy(file_src, file_target):
+    shutil.copy(file_src, file_target)
+    print(f"copy attempt: {file_src}, {file_target}")
+
+def file_delete(file_target):
+    shutil.rmtree(file_target)
+    print(f"deleting {file_target}")
+
+
+
     
 
 
